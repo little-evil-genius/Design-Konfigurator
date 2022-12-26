e26a1cc727f139a7854583fcd6f727516c86fb27
Lara <75126217+little-evil-genius@users.noreply.github.com>
2022-12-26 13:44:14 +0100

    Create designconfigurator.lang.php

---
+++ inc/languages/deutsch_sie/admin/designconfigurator.lang.php
@@ -0,0 +1,98 @@
+<?php
+// Admin Rechte
+$l['designconfigurator_permission'] = "Kann den Inhalt des Design Konfigurator verwalten?";
+// Design Konfigurator
+$l['designconfigurator_manage'] = "Design Konfigurator";
+$l['designconfigurator_manage_overview_designs'] = "Alle Farb- und Headervarianten";
+$l['designconfigurator_manage_overview_designs_desc'] = "Hier findest du eine Übersicht von den Designs die verschiedene Farb- und Headervarianten haben.";
+$l['designconfigurator_manage_add_design'] = "Neue Variante hinzufügen";
+$l['designconfigurator_manage_add_design_desc'] = "Hier fügst du einem Design eine neue Farb- und Headervariante hinzu.";
+$l['designconfigurator_manage_edit_design'] = "Variante bearbeiten";
+$l['designconfigurator_manage_edit_design_desc'] = "Hier kannst du eine bestehenden Farb- und Headervariante bearbeiten.";
+$l['designconfigurator_manage_overview_mode'] = "Alle Light-/Darkmode";
+$l['designconfigurator_manage_overview_mode_desc'] = "Hier findest du eine Übersicht von den Designs die einen Light-/Darkmodus haben.";
+$l['designconfigurator_manage_add_mode'] = "Light-/Darkmode hinzufügen";
+$l['designconfigurator_manage_add_mode_desc'] = "Hier fügst du einem Design einen Light-/Darkmodus hinzu.";
+$l['designconfigurator_manage_edit_mode'] = "Light-/Darkmode bearbeiten";
+$l['designconfigurator_manage_edit_mode_desc'] = "Hier kannst du ein bestehendes Light-/Darkmode bearbeiten.";
+$l['designconfigurator_manage_overview_accentcolor'] = "Alle Designs mit Akzentfarbe(n)";
+$l['designconfigurator_manage_overview_accentcolor_desc'] = "Hier findest du eine Übersicht von den Designs, welche eine oder mehre individuelle Design-Akzentfarbe(n) haben. Hier werden die Standardfarbe(n) festgelegt und die User können sich individuell die Farben zurechtlegen.";
+$l['designconfigurator_manage_add_accentcolor'] = "Design mit Akzentfarbe(n) hinzufügen";
+$l['designconfigurator_manage_add_accentcolor_desc'] = "Hier fügst du einem Design die Standard Akzentfarbe(n) hinzu.";
+$l['designconfigurator_manage_edit_accentcolor'] = "Standard-Aktzentfarbe(n) bearbeiten";
+$l['designconfigurator_manage_edit_accentcolor_desc'] = "Hier kannst du die Standard-Aktzentfarbe(n) von dem Design bearbeiten.";
+
+$l['designconfigurator_manage_designname'] = "Designname";
+$l['designconfigurator_manage_theme'] = "Themen ID";
+$l['designconfigurator_manage_light'] = "Light Root";
+$l['designconfigurator_manage_dark'] = "Dark Root";
+$l['designconfigurator_manage_headerimage'] = "Vorschau der Variante";
+$l['designconfigurator_manage_modeoption'] = "Light-/Darkmode";
+$l['designconfigurator_manage_defaultmode'] = "Standard Mode";
+$l['designconfigurator_manage_user'] = "Benutzer";
+$l['designconfigurator_manage_individualcolors'] = "Design-Akzentfarbe(n)";
+$l['designconfigurator_manage_options'] = "Optionen";
+// Optionsbutton
+$l['designconfigurator_manage_edit'] = "Bearbeiten";
+$l['designconfigurator_manage_delete'] = "Löschen";
+$l['designconfigurator_manage_error_invalid'] = "Ungültige Option";
+$l['designconfigurator_manage_delete_page'] = "Soll diese Designoption wirklich gelöscht werden?";
+
+// Hinzufügen und Bearbeiten
+$l['designconfigurator_manage_error_no_name'] = "Es wurde kein Designname angegeben.";
+$l['designconfigurator_manage_error_no_designID'] = "Es wurde keine Style-ID angegeben.";
+$l['designconfigurator_manage_error_no_headerimage'] = "Es wurde kein Headerlink angegeben.";
+$l['designconfigurator_manage_error_no_accentcolor1'] = "Es wurde keine erste Aktzentfarbe eingetragen.";
+$l['designconfigurator_manage_error_no_accentcolor2'] = "Es wurde keine zweite Aktzentfarbe eingetragen.";
+$l['designconfigurator_manage_error_no_root'] = "Mindestens ein Root-Verzeichnis muss angelegt werden.";
+$l['designconfigurator_manage_error_no_light_root'] = "Der Inhalt für das Root-Verzeichnis der hellen Version ist leer.";
+$l['designconfigurator_manage_error_no_dark_root'] = "Der Inhalt für das Root-Verzeichnis der dunklen Version ist leer.";
+$l['designconfigurator_manage_error_no_rootdef'] = "Es muss noch ausgewählt werden, welcher Modus der Standardmodus vom Design ist.";
+$l['designconfigurator_manage_error_no_individualcolors'] = "Es muss noch mindestens eine Aktzentfarbe angegeben werden.";
+
+$l['designconfigurator_manage_design_name_title'] = "Designname";
+$l['designconfigurator_manage_design_name_desc'] = "Gib hier ein Schlagwort für das Design an. Vermeide Leerzeichen und Sonderzeichen. Benutze _ oder - für Leerzeichen.";
+$l['designconfigurator_manage_design_designID_title'] = "Theme-ID";
+$l['designconfigurator_manage_design_designID_desc'] = "Wähle hier aus für welches Theme, die neue Farb- und Headervariante angelegt werden soll.";
+$l['designconfigurator_manage_mode_designID_desc'] = "Wähle hier aus für welches Theme, der neue Light-/Darkmodus angelegt werden soll.";
+$l['designconfigurator_manage_accentcolor_designID_desc'] = "Wähle hier aus für welches Theme, die neue Standard-Aktzenfarbe angelegt werden soll.";
+$l['designconfigurator_manage_design_headerimage_title'] = "Headergrafik";
+$l['designconfigurator_manage_design_headerimage_desc'] = "Gib hier den Bildpfad zur Headergrafik an. Den Forenurl brauchst du nicht mit angeben. <b>Beispiel:</b> images/sunset/sunset_header.png";
+$l['designconfigurator_manage_design_accentcolor1_title'] = "erste Aktzentfarbe";
+$l['designconfigurator_manage_design_accentcolor1_desc'] = "Gib hier die erste Aktzenfarbe an. Diese Farbe sollte in dieser Farbvariante am meisten dominieren. Die Aktzenfarben dienen für eine bessere Vorstellungen beim späterem auswählen.";
+$l['designconfigurator_manage_design_accentcolor2_title'] = "zweite Aktzentfarbe";
+$l['designconfigurator_manage_design_accentcolor2_desc'] = "Gib hier die zweite Aktzenfarbe an. Diese Farbe sollte in dieser Farbvariante am zweitmeisten dominieren. Die Aktzenfarben dienen für eine bessere Vorstellungen beim späteren auswählen.";
+$l['designconfigurator_manage_design_lightmode_title'] = "Lightmode";
+$l['designconfigurator_manage_design_lightmode_desc'] = "Füge hier das Root-Verzeichnis ein, welches für die helle Version genutzt werden soll. <b>Wichtig:</b> :root { INHALT }";
+$l['designconfigurator_manage_design_darkmode_title'] = "Darkmode";
+$l['designconfigurator_manage_design_darkmode_desc'] = "Füge hier das Root-Verzeichnis ein, welches für die dunkle Version genutzt werden soll. <b>Wichtig:</b> :root { INHALT }";
+$l['designconfigurator_manage_design_root_title'] = "Standardmode";
+$l['designconfigurator_manage_design_root_desc'] = "Welches Root Verzeichnis soll als Standard fungieren? Dieses wird verwendet, wenn der User nicht explizit ein Mode ausgewählt hat. Und auch Gäste sehen diese Variante.";
+$l['designconfigurator_manage_design_path_title'] = "Pfad zu den Bildern";
+$l['designconfigurator_manage_design_path_desc'] = "Wo befinden sich die Bilder für diese Farb- und Headervariante? Wenn das Feld leer gelassen wird, wird der Pfad aus dem Themes gezogen.";
+$l['designconfigurator_manage_accentcolor_individualcolors_title'] = "Standard-Aktzentfarbe(n)";
+$l['designconfigurator_manage_accentcolor_individualcolors_desc'] = "Trage hier die Standard-Aktzentfarbe(n) ein. Wenn du mehre Aktzentfarben hast, dann trenne die einzelnen #Hexcodes mit einem , und Leerzeichen <b>(, )</b>.";
+$l['designconfigurator_manage_design_root_select_def'] = "Standardmode auswählen";
+$l['designconfigurator_manage_design_root_select_light'] = "Standard: Lightmode";
+$l['designconfigurator_manage_design_root_select_dark'] = "Standard: darkmode";
+
+$l['designconfigurator_manage_submit_add'] = "Variante hinzufügen";
+$l['designconfigurator_manage_submit_add_mode'] = "Light-/Darkmode hinzufügen";
+$l['designconfigurator_manage_submit_add_accentcolor'] = "Aktzenfarbe hinzufügen";
+$l['designconfigurator_manage_submit_edit'] = "Variante bearbeiten";
+$l['designconfigurator_manage_submit_edit_mode'] = "Light-/Darkmode bearbeiten";
+$l['designconfigurator_manage_submit_edit_accentcolor'] = "Aktzenfarbe bearbeiten";
+
+$l['designconfigurator_manage_design_added'] = "neue Farb- und Headervariante hinzugefügt";
+$l['designconfigurator_manage_design_edited'] = "Farb- und Headervariante bearbeitet";
+$l['designconfigurator_manage_design_deleted'] = "Farb- und Headervariante erfolgreich gelöscht";
+$l['designconfigurator_manage_design_setdefault'] = "Neue Standard Farb- und Headervariante gesetzt";
+
+$l['designconfigurator_manage_mode_added'] = "neue Light-/Darkmode hinzugefügt";
+$l['designconfigurator_manage_mode_edited'] = "Light-/Darkmode bearbeitet";
+$l['designconfigurator_manage_mode_deleted'] = "Light-/Darkmode erfolgreich gelöscht";
+
+$l['designconfigurator_manage_accentcolor_added'] = "neues Design mit Akzentfarbe hinzugefügt";
+$l['designconfigurator_manage_accentcolor_edited'] = "Design mit Akzentfarbe bearbeitet";
+$l['designconfigurator_manage_accentcolor_deleted'] = "Design mit Akzentfarbe erfolgreich gelöscht";
+?>
