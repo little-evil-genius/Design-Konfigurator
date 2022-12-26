04772d0b117caa482350e8a04659305d01728888
Lara <75126217+little-evil-genius@users.noreply.github.com>
2022-12-26 13:43:19 +0100

    Create designconfigurator.lang.php

---
+++ inc/languages/deutsch_du/designconfigurator.lang.php
@@ -0,0 +1,33 @@
+<?php
+$l['designconfigurator_usercp'] = "Design Konfiguration";
+$l['designconfigurator_usercpmenu_nav'] = "Design anpassen";
+$l['designconfigurator_usercp_desc'] = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+$l['designconfigurator_usercp_mode'] = "Light- und Darkmodus";
+$l['designconfigurator_usercp_design'] = "Header-/Farbkombination";
+$l['designconfigurator_usercp_accentcolors'] = "individuelle Anpassungen";
+$l['designconfigurator_accentcolor'] = "Design-Farbe";
+$l['designconfigurator_accentcolor_desc'] = "Trage hier eine für dich individuelle Akzent-Farbe ein.";
+$l['designconfigurator_accentcolor_def'] = "Standard-Aktzenfarbe Nr.";
+$l['designconfigurator_accentcolor_button'] = "Aktzenfarbe speichern";
+$l['designconfigurator_accentcolors_button'] = "Aktzenfarben speichern";
+$l['designconfigurator_accentcolor_none'] = "Für das von dir aktuelle ausgewählte Design gibt es nicht die Möglichkeit eine oder mehre indiviuelle Design-Aktzentfarben festzulegen.";
+$l['designconfigurator_accentcolor_own_number'] = "Eigene Aktzenfarbe Nr.";
+$l['designconfigurator_lightdarkmode_none_light'] = "Lightmodus";
+$l['designconfigurator_lightdarkmode_none_dark'] = "Darkmodus";
+$l['designconfigurator_designswitch_none'] = "Für das von dir aktuell ausgewählte Design gibt es keine verschiedenen Header-/Farbkombination!";
+$l['designconfigurator_lightdarkmode_none_mode'] = "Für das von dir aktuell ausgewählte Design gibt es nicht die Option zwischen einem Light- oder Darkmodus zu wechseln! <br>Vom Team wird ein <b>{1} Design</b> angeboten.";
+$l['designconfigurator_lightdarkmode_none'] = "Für das von dir aktuell ausgewählte Design gibt es nicht die Option zwischen einem Light- oder Darkmodus zu wechseln!";
+$l['switcher_lightdarkbutton_light'] = "Lightmode aktivieren";
+$l['switcher_lightdarkbutton_dark'] = "Darkmode aktivieren";
+$l['designconfigurator_lightdark_link_light'] = "<i class=\"fa-solid fa-sun\"></i> Lightmodus";
+$l['designconfigurator_lightdark_link_dark'] = "<i class=\"fa-solid fa-moon\"></i> Darkmodus";
+$l['designconfigurator_lightdark_info_light'] = "<i class=\"fa-solid fa-sun\"></i> Light";
+$l['designconfigurator_lightdark_info_dark'] = "<i class=\"fa-solid fa-moon\"></i> Dark";
+$l['designconfigurator_redirect_designswitch'] = "Du hast erfolgreich eine neue Farb-/Headerkombination für dich festgelegt. Du wirst nun auf die Design Konfigurationsseite zurückgeleitet!";
+$l['designconfigurator_redirect_designdimm'] = "Du hast erfolgreich dein Helligkeitsmodus festgelegt. Du wirst nun auf die Design Konfigurationsseite zurückgeleitet!";
+$l['designconfigurator_redirect_accentcolor'] = "Du hast erfolgreich deine individuellen Anpassungen gespeichert. Du wirst nun auf die Design Konfigurationsseite zurückgeleitet!";
+$l['designconfigurator_online_location'] = "Passt sich gerade im User-CP das <a href=\"usercp.php?action=designconfigurator\">Design</a> an.";
+
+$l['designconfigurator_newDesign'] = "Für das Design <b>{3}</b> gibt es eine neue Farb-/Header Kombination.";
+$l['myalerts_setting_designconfigurator_newDesign'] = "Benachrichtigung erhalten, wenn eine neue Farb-/Headervariante hinzugefügt wird?";
+?>
