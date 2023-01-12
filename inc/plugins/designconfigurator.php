06152ca57e8a20b2b3d63e0c6f2cdd8698ba6540
Lara <75126217+little-evil-genius@users.noreply.github.com>
2023-01-12 11:58:30 +0100

    Update designconfigurator.php

--- inc/plugins/designconfigurator.php
+++ inc/plugins/designconfigurator.php
@@ -28,7 +28,7 @@ function designconfigurator_info() {
 		"website" => "https://github.com/little-evil-genius/Design-Konfigurator",
 		"author" => "little.evil.genius",
 		"authorsite" => "https://storming-gates.de/member.php?action=profile&uid=1712",
-		"version" => "1.0",
+		"version" => "1.1",
 		"compatibility" => "18*"
 	);
 }
