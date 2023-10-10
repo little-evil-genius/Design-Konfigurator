b12eb84cc3aa2092405b9f3c0fe0e60ae3ee6760
Lara <75126217+little-evil-genius@users.noreply.github.com>
2023-10-10 19:46:52 +0200

    Update designconfigurator.php

--- inc/plugins/designconfigurator.php
+++ inc/plugins/designconfigurator.php
@@ -3140,7 +3140,18 @@ function designconfigurator_headerinclude() {
 
 					$root_accentcolor = "--accent".$number.": ".$individualcolor.";";
 
-					$allroot_colors .= $root_accentcolor."\n";
+					// RGB FARBEN
+					list($r, $g, $b) = sscanf($individualcolor, "#%02x%02x%02x");
+					$rgb_color = "$r $g $b";
+				
+					$opacity  = 0;
+					$root_accentcolor_rgb = "";
+					for ($i = 0; $i < 9; $i++) {
+						$opacity  ++;
+						$root_accentcolor_rgb .= "--accent".$number."_rgb".$opacity .": rgb(".$rgb_color."/ 0.".$opacity .");\n";
+					}
+
+					$allroot_colors .= $root_accentcolor."\n".$root_accentcolor_rgb."\n";
 				}
 
 			} else {
@@ -3168,12 +3179,10 @@ function designconfigurator_headerinclude() {
 					for ($i = 0; $i < 9; $i++) {
 						$opacity  ++;
 						$root_accentcolor_rgb .= "--accent".$number."_rgb".$opacity .": rgb(".$rgb_color."/ 0.".$opacity .");\n";
-				
 					}
 
 					$allroot_colors .= $root_accentcolor."\n".$root_accentcolor_rgb."\n";
 				}
-
 			}
 
 			$rootsystem = "<style type=\"text/css\">:root {\n".$allroot_colors."}\n".$designroot."</style>";
@@ -3233,7 +3242,7 @@ function designconfigurator_usercp() {
 	$additionalgroups  = $mybb->user['additionalgroups'];
 
 	if ($mybb->input['action'] == "designconfigurator") {
-		
+
 		add_breadcrumb($lang->nav_usercp, "usercp.php");
         add_breadcrumb($lang->designconfigurator_usercp, "usercp.php?action=designconfigurator");
 
